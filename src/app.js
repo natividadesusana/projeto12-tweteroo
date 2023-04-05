e4edd485c45992e16bf9ce3efbdaeacfa564a7d9
natividadesusana <susanajdsn@gmail.com>
2023-04-05 16:36:32 -0300

    creating POST /tweets

--- src/app.js
+++ src/app.js
@@ -7,10 +7,15 @@ app.use(express.json());
 
 const users = [
   {
-    username:
-        "bobesponja",
-    avatar:
-        "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+    username: "bobesponja",
+    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+  },
+];
+
+const tweets = [
+  {
+    username: "bobesponja",
+    tweet: "Eu amo hambúrguer de siri!",
   },
 ];
 
@@ -19,14 +24,31 @@ app.post("/sign-up", (req, resp) => {
 
   if (username && avatar) {
     users.push(req.body);
-    return resp
-        .status(201)
-        .send("OK");
+    return resp.status(201).send("OK");
   }
   return resp
     .status(400)
     .send("O nome de usuário e o avatar são obrigatórios!");
 });
+
+app.post("/tweets", (req, resp) => {
+    const { username, tweet } = req.body;
+  
+    const userExists = users.find(user => user.username === username);
+  
+    if (!userExists) {
+      return resp
+      .status(401)
+      .send("UNAUTHORIZED");
+    }
+  
+    const newTweet = { username, tweet };
+    tweets.push(newTweet);
+    console.log(tweets);
+  
+    return resp.send("OK");
+  });
+  
 
 const DOOR = 5000;
 app.listen(DOOR, () => console.log(`Servidor rodando na porta ${DOOR} ...`));
