692b9974775efffd66f3f41843a2969064e034d4
natividadesusana <susanajdsn@gmail.com>
2023-04-06 15:53:40 -0300

    merging master code to main

--- src/app.js
+++ src/app.js
@@ -1,32 +1,60 @@
 import express from "express";
 import cors from "cors";
+import Joi from "joi";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [
-  {
-    username:
-        "bobesponja",
-    avatar:
-        "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-  },
-];
+const users = [];
+const tweets = [];
 
-app.post("/sign-up", (req, resp) => {
-  const { username, avatar } = req.body;
-
-  if (username && avatar) {
-    users.push(req.body);
-    return resp
-        .status(201)
-        .send("OK");
-  }
-  return resp
-    .status(400)
-    .send("O nome de usuário e o avatar são obrigatórios!");
+const schema = Joi.object({
+  username: Joi.string().required(),
+  avatar: Joi.string().required(),
 });
 
-const DOOR = 5000;
+app.post("/sign-up", (req, resp) => {
+  const { error } = schema.validate(req.body);
+
+  if (error) {
+    resp.status(400).send("Todos os campos são obrigatórios!");
+    return
+  }
+
+  users.push(req.body);
+
+  return resp.status(201).send("OK");
+});
+
+app.post("/tweets", (req, resp) => {
+  const username = req.header('user');
+  const tweet = req.body.tweet;
+
+  const isUserRegistered = users.find((user) => user.username === username);
+
+  if (!isUserRegistered) {
+    resp.status(400).send("UNAUTHORIZED");
+    return
+  }
+
+  tweets.push({ username, tweet });
+  console.log(`Novo tweet de ${username}: ${tweet}`);
+
+  return resp.status(201).send("OK");
+});
+
+app.get("/tweets", (req, resp) => {
+  const latestTweets = tweets.slice(-10);
+
+  const tweetAvatars = latestTweets.map((tweet) => {
+    const user = users.find((user) => user.username === tweet.username);
+    const avatar = user ? user.avatar : null;
+    return { ...tweet, avatar };
+  });
+
+  return resp.status(200).send(tweetAvatars);
+});
+
+const DOOR = 3000;
 app.listen(DOOR, () => console.log(`Servidor rodando na porta ${DOOR} ...`));
